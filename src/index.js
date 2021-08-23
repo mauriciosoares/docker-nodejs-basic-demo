76ee314ebdcb9772e20db1bda8a357ac6dc8332a
Mauricio Soares <m.soares2603@gmail.com>
2021-08-23 13:48:57 -0300

    update port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ app.get('/', (req, res ) =>
     res.json({ message: 'Docker and ECS are super easy 🐳 update, CPU change low health percentage, another deployment!' }) 
 );
 
-const port = process.env.PORT || 8080;
+const port = 21096;
 
 app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
